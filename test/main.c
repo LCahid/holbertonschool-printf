43d786c573cc36f4ef15e94a5fc7ebe779e1f550
Cahid <mr.cebizade@gmail.com>
2023-11-27 07:41:56 -0800

    0

--- test/main.c
+++ test/main.c
@@ -9,9 +9,13 @@
  */
 int main(void)
 {
+	_printf("%%");
+	printf("%%");
+	/**_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
 _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");*/
 return (0);
 }
