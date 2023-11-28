9fc7c1c90c52a19d2dddfa679db77ba2c0acd41b
Elvinooooo <Elvinooooo@github.com>
2023-11-28 04:07:23 -0800

    upd

--- test/_printf.c
+++ test/_printf.c
@@ -3,6 +3,46 @@
 #include <stdarg.h>
 #include "main.h"
 /**
+ * handle_print - function that handles printing different data types
+ * @format: Pointer to the format
+ * @i: the iterator for the format iteration
+ * Return: the size
+ */
+int handle_print(const char *format, int *i, va_list *el)
+{
+	int size = 0;
+	char *str;
+
+ 	switch (format[++(*i)])
+	{
+		case 'c':
+			_putchar(va_arg(*el, int));
+			size++;
+			break;
+		case 's':
+			str = va_arg(*el, char *);
+			str == NULL ? str = "(null)" : str;
+			size += write(1, str, _strlen(str));
+			break;
+		case '%':
+			_putchar('%'), size++;
+			break;
+		case 'd':
+		case 'i':
+			_putchar(va_arg(*el, int));
+			size++;
+			break;
+		case '\0':
+			i--;
+			break;
+		default:
+			_putchar('%');
+			size = size + _printf("%c", format[*i]) + 1;
+			break;
+	}	
+	return (size);
+}
+/**
  * _printf - Entry point
  *
  * @format: format str
@@ -13,7 +53,6 @@ int _printf(const char *format, ...)
 {
 	va_list el;
 	int i, size = 0;
-	char *str;
 
 	va_start(el, format);
 	for (i = 0; format && format[i]; i++)
@@ -23,30 +62,7 @@ int _printf(const char *format, ...)
 			size++;
 		}
 		else
-		{
-			switch (format[++i])
-			{
-				case 'c':
-					_putchar(va_arg(el, int));
-					size++;
-					break;
-				case 's':
-					str = va_arg(el, char *);
-					str == NULL ? str = "(null)" : str;
-					size += write(1, str, _strlen(str));
-					break;
-				case '%':
-					_putchar('%'), size++;
-					break;
-				case '\0':
-					i--;
-					break;
-				default:
-					_putchar('%');
-					size = size + _printf("%c", format[i]) + 1;
-					break;
-			}
-		}
+			size += handle_print(format, &i, &el);
 	va_end(el);
 	if (size == 0)
 		return (-1);
